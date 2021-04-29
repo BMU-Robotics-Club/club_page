33bf23069988e9f39bd24466bf25988c304000e1
harshithtunuguntla <harshithtunuguntla@gmail.com>
2021-04-29 16:27:38 +0530

    b&w in events page

--- js/events.js
+++ js/events.js
@@ -1,22 +1,60 @@
 function showupcomingevents(){
     document.getElementById("upcominginformation").style.display = "flex";
     document.getElementById("flagshipinformation").style.display = "none";
-   // document.getElementById("eventsmenu1").style.backgroundColor = "blue";
-    //document.getElementById("classforbutton1").style.backgroundColor = "blue";
-    //document.getElementById("upcominginformation").style.backgroundColor = "blue";
-    document.getElementById("classforbutton1").style.color="white";
-    document.getElementById("classforbutton2").style.color="black";
+    document.getElementById("workshopinformation").style.display = "none";
+    document.getElementById("technicalinformation").style.display = "none";
 
-
+    document.getElementById("smallvertical1").style.display = "flex";
+    document.getElementById("smallvertical2").style.display = "none";
+    document.getElementById("smallvertical3").style.display = "none";
+    document.getElementById("smallvertical4").style.display = "none";
 }
 
 function showflagshipevents(){
     document.getElementById("upcominginformation").style.display = "none";
     document.getElementById("flagshipinformation").style.display = "flex";
+    document.getElementById("workshopinformation").style.display = "none";
+    document.getElementById("technicalinformation").style.display = "none";
 
-    document.getElementById("classforbutton1").style.color="black";
-    document.getElementById("classforbutton2").style.color="white";
+   
+
+     document.getElementById("smallvertical1").style.display = "none";
+     document.getElementById("smallvertical2").style.display = "flex";
+    document.getElementById("smallvertical3").style.display = "none";
+    document.getElementById("smallvertical4").style.display = "none";
 }
+
+function showworkshops(){
+    document.getElementById("upcominginformation").style.display = "none";
+    document.getElementById("flagshipinformation").style.display = "none";
+    document.getElementById("workshopinformation").style.display = "flex";
+    document.getElementById("technicalinformation").style.display = "none";
+
+    document.getElementById("smallvertical1").style.display = "none";
+    document.getElementById("smallvertical2").style.display = "none";
+    document.getElementById("smallvertical3").style.display = "flex";
+    document.getElementById("smallvertical4").style.display = "none";
+}
+
+function showtechnicalevents(){
+    document.getElementById("upcominginformation").style.display = "none";
+    document.getElementById("flagshipinformation").style.display = "none";
+    document.getElementById("workshopinformation").style.display = "none";
+    document.getElementById("technicalinformation").style.display = "flex";
+
+    document.getElementById("smallvertical1").style.display = "none";
+    document.getElementById("smallvertical2").style.display = "none";
+    document.getElementById("smallvertical3").style.display = "none";
+    document.getElementById("smallvertical4").style.display = "flex";
+}
+
+
+
+
+
+
+
+
 
 var i = 0;
 var txt = 'Lets !! Get !! Chase !!'; /* The text */
