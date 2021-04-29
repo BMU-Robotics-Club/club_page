407c526fa69f61be66631cd1e1af3947cca5b945
sishir2001 <jaladisishir96@gmail.com>
2021-04-29 16:06:55 +0530

    Projects-part-done

--- js/index2.js
+++ js/index2.js
@@ -87,12 +87,12 @@ let flex_img = document.getElementById("flex-img");
 let flex_content = document.getElementById("flex-content");
 
 function FBbot(){
-  flex_img.setAttribute("src","/Images/food.jpg");
+  flex_img.setAttribute("src","/Images/foodServing.jpg");
   flex_content.innerHTML = "<h4>By Charith Reddy</h4><h4>Batch: 2021 </h4><h4>About : </h4><p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:</p>";
 }
 
 function FFbot(){
-  flex_img.setAttribute("src","/Images/firefighting.jpg");
+  flex_img.setAttribute("src","/Images/firefighting.webp");
   flex_content.innerHTML = "<h4>By Harshith Tunuguntla</h4><h4>Batch: 2023 </h4><h4>About : </h4><p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:</p>";
 }
 
