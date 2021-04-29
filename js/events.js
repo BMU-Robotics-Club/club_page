41e3ec01697b94847f2306046b01714c018f1e91
harshithtunuguntla <harshithtunuguntla@gmail.com>
2021-04-29 13:53:03 +0530

    events page made relative

--- js/events.js
+++ js/events.js
@@ -1,10 +1,10 @@
 function showupcomingevents(){
-    document.getElementById("upcominginformation").style.display = "inline";
+    document.getElementById("upcominginformation").style.display = "flex";
     document.getElementById("flagshipinformation").style.display = "none";
    // document.getElementById("eventsmenu1").style.backgroundColor = "blue";
     //document.getElementById("classforbutton1").style.backgroundColor = "blue";
     //document.getElementById("upcominginformation").style.backgroundColor = "blue";
-    document.getElementById("classforbutton1").style.color="red";
+    document.getElementById("classforbutton1").style.color="white";
     document.getElementById("classforbutton2").style.color="black";
 
 
@@ -12,8 +12,26 @@ function showupcomingevents(){
 
 function showflagshipevents(){
     document.getElementById("upcominginformation").style.display = "none";
-    document.getElementById("flagshipinformation").style.display = "inline";
+    document.getElementById("flagshipinformation").style.display = "flex";
 
     document.getElementById("classforbutton1").style.color="black";
-    document.getElementById("classforbutton2").style.color="red";
+    document.getElementById("classforbutton2").style.color="white";
 }
+
+var i = 0;
+var txt = 'Lets !! Get !! Chase !!'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+
+
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+
+
+}
+
