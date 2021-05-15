8c9e12d047a01a9eaeeff355ce529eb9466fd56a
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-05-15 19:07:50 +0530

    cleaning

--- js/Userdata_signup.js
+++ js/Userdata_signup.js
@@ -2,6 +2,28 @@ userdata=[];
 
 function storedata(){
 
-var pass1=document.getElementById("phone").Value;
-alert(pass1);
+var mail=document.getElementById("mail").value;
+var phone=document.getElementById("phone").value;
+var pass1=document.getElementById("pass1").value;
+var pass2=document.getElementById("pass2").value;
+if(pass1 != pass2)
+{
+    alert("Passwords are not same!");
+}
+else{
+//   DATAstorage
+}
+
+
+}
+
+
+
+function loginvalidate()
+{
+// login validation
+
+    alert("hi");
+
+    
 }
