eaf0bd9733567e39507664816a24f207c64c2f65
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-05-19 17:39:35 +0530

    new blog design

---
+++ New_Blog_Desgin/NewBlog.js
@@ -0,0 +1,29 @@
+
+// Blog1
+function show(){
+    var element=document.getElementById("readit1");
+    element.style.display="block";
+
+   
+}
+
+function unshow(){
+   
+    var element=document.getElementById("readit1");
+    element.style.display="none";
+
+}
+
+function showwho(){
+
+    var element2=document.getElementById("who1");
+    element2.style.display="block";
+
+}
+
+function unshowwho(){
+
+    var element2=document.getElementById("who1");
+    element2.style.display="none";
+
+}
