257a6d8c9f6a04e2fe5ec12a98227b22d964ea58
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-05-19 18:10:58 +0530

    blog

--- New_Blog_Desgin/NewBlog.js
+++ New_Blog_Desgin/NewBlog.js
@@ -27,3 +27,61 @@ function unshowwho(){
     element2.style.display="none";
 
 }
+
+// Blog2
+function show2(){
+    var element=document.getElementById("readit2");
+    element.style.display="block";
+
+   
+}
+
+function unshow2(){
+   
+    var element=document.getElementById("readit2");
+    element.style.display="none";
+
+}
+
+function showwho2(){
+
+    var element2=document.getElementById("who2");
+    element2.style.display="block";
+
+}
+
+function unshowwho2(){
+
+    var element2=document.getElementById("who2");
+    element2.style.display="none";
+
+}
+
+// Blog3
+function show3(){
+    var element=document.getElementById("readit3");
+    element.style.display="block";
+
+   
+}
+
+function unshow3(){
+   
+    var element=document.getElementById("readit3");
+    element.style.display="none";
+
+}
+
+function showwho3(){
+
+    var element2=document.getElementById("who3");
+    element2.style.display="block";
+
+}
+
+function unshowwho3(){
+
+    var element2=document.getElementById("who3");
+    element2.style.display="none";
+
+}
