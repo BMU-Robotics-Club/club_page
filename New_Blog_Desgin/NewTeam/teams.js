d2ee5e83bcd6ba9d91c796c65a77a8a226443774
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-05-22 16:50:53 +0530

    done js

---
+++ New_Blog_Desgin/NewTeam/teams.js
@@ -0,0 +1,115 @@
+function one()
+{
+
+var board1=document.getElementById("board1");
+board1.style.display="block";
+board1.style.opacity=1;
+
+}
+function oneout()
+{
+
+var board1=document.getElementById("board1");
+board1.style.opacity=0;
+board1.style.display="none";
+
+
+}
+
+function two()
+{
+   
+
+var board2=document.getElementById("board2");
+board2.style.display="block";
+board2.style.opacity=1;
+
+}
+function twoout()
+{
+
+var board2=document.getElementById("board2");
+board2.style.opacity=0;
+board2.style.display="none";
+
+
+}
+
+function three()
+{
+   
+
+var board3=document.getElementById("board3");
+board3.style.display="block";
+board3.style.opacity=1;
+
+}
+function threeout()
+{
+
+var board3=document.getElementById("board3");
+board3.style.opacity=0;
+board3.style.display="none";
+
+
+}
+
+
+function four()
+{
+   
+
+var boar4=document.getElementById("board4");
+board4.style.display="block";
+board4.style.opacity=1;
+
+}
+function fourout()
+{
+
+var board4=document.getElementById("board4");
+board4.style.opacity=0;
+board4.style.display="none";
+
+
+}
+
+
+function five()
+{
+   
+
+var board5=document.getElementById("board5");
+board5.style.display="block";
+board5.style.opacity=1;
+
+}
+function fiveout()
+{
+
+var board5=document.getElementById("board5");
+board5.style.opacity=0;
+board5.style.display="none";
+
+
+}
+
+
+function six()
+{
+   
+
+var board6=document.getElementById("board6");
+board6.style.display="block";
+board6.style.opacity=1;
+
+}
+function sixout()
+{
+
+var board6=document.getElementById("board6");
+board6.style.opacity=0;
+board6.style.display="none";
+
+
+}
