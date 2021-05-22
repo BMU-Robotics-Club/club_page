9234f085a0b2161e46bf2862491f3d7b04d32b97
harshithtunuguntla <harshithtunuguntla@gmail.com>
2021-05-23 01:45:54 +0530

    events page completetion wthout buttons functionality in image 2

--- js/events.js
+++ js/events.js
@@ -1,79 +1,34 @@
-function closeupcomingevents(){
-  document.getElementById("upcominginformation").style.display = "none";
+
+function closeall(){
   document.getElementById("statsdiv").style.display = "inline-block";
+  document.getElementById("upcominginformation").style.display = "none";
+  document.getElementById("flagshipinformation").style.display = "none";
+  document.getElementById("workshopinformation").style.display = "none";
+  document.getElementById("technicalinformation").style.display = "none";
+  
 }
+
 
 function openupcomingevents(){
-  document.getElementById("upcominginformation").style.display = "inline-block";
+
   document.getElementById("statsdiv").style.display = "none";
-}
-
-function showupcomingevents(){
-    document.getElementById("upcominginformation").style.display = "flex";
-    document.getElementById("flagshipinformation").style.display = "none";
-    document.getElementById("workshopinformation").style.display = "none";
-    document.getElementById("technicalinformation").style.display = "none";
-
-    document.getElementById("smallvertical1").style.display = "flex";
-    document.getElementById("smallvertical2").style.display = "none";
-    document.getElementById("smallvertical3").style.display = "none";
-    document.getElementById("smallvertical4").style.display = "none";
-}
-
-function showflagshipevents(){
-    document.getElementById("upcominginformation").style.display = "none";
-    document.getElementById("flagshipinformation").style.display = "flex";
-    document.getElementById("workshopinformation").style.display = "none";
-    document.getElementById("technicalinformation").style.display = "none";
-
-   
-
-     document.getElementById("smallvertical1").style.display = "none";
-     document.getElementById("smallvertical2").style.display = "flex";
-    document.getElementById("smallvertical3").style.display = "none";
-    document.getElementById("smallvertical4").style.display = "none";
-}
-
-function showworkshops(){
-    document.getElementById("upcominginformation").style.display = "none";
-    document.getElementById("flagshipinformation").style.display = "none";
-    document.getElementById("workshopinformation").style.display = "flex";
-    document.getElementById("technicalinformation").style.display = "none";
-
-    document.getElementById("smallvertical1").style.display = "none";
-    document.getElementById("smallvertical2").style.display = "none";
-    document.getElementById("smallvertical3").style.display = "flex";
-    document.getElementById("smallvertical4").style.display = "none";
-}
-
-function showtechnicalevents(){
-    document.getElementById("upcominginformation").style.display = "none";
-    document.getElementById("flagshipinformation").style.display = "none";
-    document.getElementById("workshopinformation").style.display = "none";
-    document.getElementById("technicalinformation").style.display = "flex";
-
-    document.getElementById("smallvertical1").style.display = "none";
-    document.getElementById("smallvertical2").style.display = "none";
-    document.getElementById("smallvertical3").style.display = "none";
-    document.getElementById("smallvertical4").style.display = "flex";
+  document.getElementById("upcominginformation").style.display = "inline-block";
+  document.getElementById("flagshipinformation").style.display = "none";
+  document.getElementById("workshopinformation").style.display = "none";
+  document.getElementById("technicalinformation").style.display = "none";
+  
+  
 }
 
 
-
-var i = 0;
-var txt = 'Lets !! Get !! Chase !!'; /* The text */
-var speed = 150; /* The speed/duration of the effect in milliseconds */
-
-
-
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-
-
+function openflagshipevents(){
+  document.getElementById("upcominginformation").style.display = "none";
+  document.getElementById("workshopinformation").style.display = "none";
+  document.getElementById("technicalinformation").style.display = "none";
+  document.getElementById("statsdiv").style.display = "none";
+  document.getElementById("flagshipinformation").style.display = "inline-block";
+  
+  
 }
+
 
