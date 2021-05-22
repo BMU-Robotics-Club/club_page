6e09ea7dee083bbc4c25c1f8216d3a6681097bee
harshithtunuguntla <harshithtunuguntla@gmail.com>
2021-05-23 02:10:15 +0530

    events page 90% Updated

--- js/events.js
+++ js/events.js
@@ -22,13 +22,30 @@ function openupcomingevents(){
 
 
 function openflagshipevents(){
+  document.getElementById("statsdiv").style.display = "none";
+  document.getElementById("flagshipinformation").style.display = "inline-block";
   document.getElementById("upcominginformation").style.display = "none";
   document.getElementById("workshopinformation").style.display = "none";
   document.getElementById("technicalinformation").style.display = "none";
+
+   
+}
+
+function openworkshopevents(){
   document.getElementById("statsdiv").style.display = "none";
-  document.getElementById("flagshipinformation").style.display = "inline-block";
-  
-  
+  document.getElementById("workshopinformation").style.display = "inline-block";
+  document.getElementById("flagshipinformation").style.display = "none";
+  document.getElementById("upcominginformation").style.display = "none";
+  document.getElementById("technicalinformation").style.display = "none";
+}
+
+function opentechnicalevents(){
+  document.getElementById("statsdiv").style.display = "none";
+  document.getElementById("technicalinformation").style.display = "inline-block";
+  document.getElementById("workshopinformation").style.display = "none";
+  document.getElementById("flagshipinformation").style.display = "none";
+  document.getElementById("upcominginformation").style.display = "none";
+
 }
 
 
