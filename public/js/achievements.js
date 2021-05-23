ae90b4eda77701bd176fcc928c49e1febe4379ea
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-05-24 03:02:09 +0530

    done

--- public/js/achievements.js
+++ public/js/achievements.js
@@ -15,21 +15,21 @@ let contentp=document.getElementById("contentp");
 // !HiddenOpen
 function HiddenOpen1(){
    sec_hidden.classList.replace("Hidden-not-active","Hidden-active");
-   contentp.innerHTML ="<h1 style='margin-left:-45%;'>1 Prize from <span style='color:blue;font-weight: bold;'>Uphoria 2019</span></h1><p style='font-weight: bold;text-align: left;margin-top: 12%;width:100%;'>The line follower robot is a mobile machine that can detect and follow the line drawn on the floor. Generally, the path is predefined and can be either visible like a black line on a white surface with a high contrasted color or it can be invisible like a magnetic filed.</p><h3 style='margin-left:-78%;margin-top:10%;color:blue;'>Team Members</h3><h4 style='margin-left:-20%;margin-top:1%;'>Charith Reddy, Gurram Dinesh, Akash. </h4>";
+   contentp.innerHTML ="<h1 style='margin-left:-45%;'>1 Prize from <span style='color:blue;font-weight: bold;'>Uphoria 2019</span></h1><p style='font-weight: bold;text-align: left;margin-top: 12%;width:100%;'>The line follower robot is a mobile machine that can detect and follow the line drawn on the floor. Generally, the path is predefined and can be either visible like a black line on a white surface with a high contrasted color or it can be invisible like a magnetic filed.</p><h3 style='margin-left:-78%;margin-top:10%;color:blue;'>Team Members</h3><h4 style='margin-left:-20%;margin-top:1%;'>Charith Reddy. </h4>";
     
 }
 
 
 function HiddenOpen2(){
 sec_hidden.classList.replace("Hidden-not-active","Hidden-active");
-contentp.innerHTML ="<h1 style='margin-left:-45%;'>1 Prize from <span style='color:blue;font-weight: bold;'>Uphoria 2019</span></h1><p style='font-weight: bold;text-align: left;margin-top: 12%;width:100%;'>Robot Soccer is a robot sport, where robot machines compete against each other in a variation of the game of Association Football (soccer). In robot soccer, each team is fully autonomous - once the game starts the only input from any human is from the referee.</p><h3 style='margin-left:-78%;margin-top:10%;color:blue;'>Team Members</h3><h4 style='margin-left:-20%;margin-top:1%;'>Charith Reddy, Gurram Dinesh, Akash. </h4>";
+contentp.innerHTML ="<h1 style='margin-left:-45%;'>1 Prize from <span style='color:blue;font-weight: bold;'>Uphoria 2019</span></h1><p style='font-weight: bold;text-align: left;margin-top: 12%;width:100%;'>Robot Soccer is a robot sport, where robot machines compete against each other in a variation of the game of Association Football (soccer). In robot soccer, each team is fully autonomous - once the game starts the only input from any human is from the referee.</p><h3 style='margin-left:-78%;margin-top:10%;color:blue;'>Team Members</h3><h4 style='margin-left:-20%;margin-top:1%;'>Akash. </h4>";
 
 }
 
 
 function HiddenOpen3(){
     sec_hidden.classList.replace("Hidden-not-active","Hidden-active");
-    contentp.innerHTML ="<h1 style='margin-left:-45%;'>1 Prize from <span style='color:blue;font-weight: bold;'>Uphoria 2019</span></h1><p style='font-weight: bold;text-align: left;margin-top: 12%;width:100%;'>Nitro vehicles also require a means of cranking the engine over, which can be achieved using a pull-start, starter box, battery operated rotostart, or by an electric drill. The Traxxas nitro rc cars come with a hand start up system, that revs the engine and starts it up. It doesn't require a pull start system.</p><h3 style='margin-left:-78%;margin-top:10%;color:blue;'>Team Members</h3><h4 style='margin-left:-20%;margin-top:1%;'>Charith Reddy, Gurram Dinesh, Akash. </h4>";
+    contentp.innerHTML ="<h1 style='margin-left:-45%;'>1 Prize from <span style='color:blue;font-weight: bold;'>Uphoria 2019</span></h1><p style='font-weight: bold;text-align: left;margin-top: 12%;width:100%;'>Nitro vehicles also require a means of cranking the engine over, which can be achieved using a pull-start, starter box, battery operated rotostart, or by an electric drill. The Traxxas nitro rc cars come with a hand start up system, that revs the engine and starts it up. It doesn't require a pull start system.</p><h3 style='margin-left:-78%;margin-top:10%;color:blue;'>Team Members</h3><h4 style='margin-left:-20%;margin-top:1%;'>Gurram Dinesh. </h4>";
     
     }
 
