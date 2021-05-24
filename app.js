f2146a720c2d0114c48c1bc26d228f2deac61391
jaladi saai sishir <jaladisishir96@gmail.com>
2021-05-24 05:48:24 +0530

    dynamic port

--- app.js
+++ app.js
@@ -238,8 +238,14 @@ app.get("/:some",(req,res)=>{
   res.redirect('/error');
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
 // ! Setting server to a port 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
 
